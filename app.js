761c65d7e072b5cf52bcae59879fd90719506245
Jose Alberto Alves <alveslemosneto@gmail.com>
2025-03-06 12:04:54 -0300

    projeto inicial

---
+++ app.js
@@ -0,0 +1,22 @@
+
+alert ("Boas vindas ao jogo do número secreto");
+let numeroSecreto = parseInt(Math.random()*100+1)
+console.log(numeroSecreto);
+let chute;
+let cont=0;
+
+while(numeroSecreto!=chute){
+    chute=prompt("Escolha um número inteiro que esteja no intervalo de 1 a 100");
+ if(numeroSecreto==chute){
+    alert(`Você acertou o número secreto que é ${numeroSecreto} e você fez isso em ${cont} tentativas`);
+ } else {
+    if(numeroSecreto>chute){
+        alert(`O número secreto é maior que ${chute}` );
+        cont=cont+1
+    } else{
+        alert(`O número secreto é menor que ${chute}`);
+        cont=cont+1
+    }
+ }
+
+}
